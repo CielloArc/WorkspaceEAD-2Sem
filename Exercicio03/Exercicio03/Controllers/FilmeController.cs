9be961f5e594c667b7b74ccb15b3c334571e0fb5
logonrm <logonrm@fiap.com.br>
2017-09-29 11:54:45 -0300

    29/SET/2017 - Exercicio concluido

---
+++ Exercicio03/Exercicio03/Controllers/FilmeController.cs
@@ -0,0 +1,49 @@
+﻿using Exercicio03.Models;
+using Exercicio03.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercicio03.Controllers
+{
+    public class FilmeController : Controller
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+
+
+        // GET: Filme
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                _unit.FilmeRepository.Cadastrar(filme);
+                _unit.Save();
+                TempData["msg"] = "Filme Cadastrado com sucesso";
+                return RedirectToAction("Listar");
+            }
+            else {
+                return View(filme);
+            }
+        }
+
+        public ActionResult Listar()
+        {
+            return View(_unit.FilmeRepository.Listar());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _unit.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Exercicio03/Exercicio03/Units/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using Exercicio03.Persistence;
+using Exercicio03.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+
+namespace Exercicio03.Units
+{
+    public class UnitOfWork : IDisposable
+    {
+        private CinemaContext _context = new CinemaContext();
+        private IFilmeRepository _filmeRepository;
+
+        public IFilmeRepository FilmeRepository
+        {
+            get
+            {
+                if (_filmeRepository == null)
+                {
+                    _filmeRepository = new FilmeRepository(_context);
+                }
+                return _filmeRepository;
+            }
+        }
+        
+
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in entityValidationErrors.ValidationErrors)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
